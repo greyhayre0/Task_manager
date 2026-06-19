31ff7d774c50d9d1e78cd09a01a18feac69ab29b
gray-haired <greyhayre1@gmail.com>
2026-06-19 04:16:39 +0300

    Update init_db.py

--- app/services/init_db.py
+++ app/services/init_db.py
@@ -2,9 +2,7 @@ from sqlalchemy.orm import Session
 from app.models import User, TaskStatus, UserRole
 from app.core.config import settings
 
-
 def init_db(db: Session):
-    
     if not db.query(User).filter(User.email == settings.ADMIN_EMAIL).first():
         admin = User(
             email=settings.ADMIN_EMAIL,
@@ -18,9 +16,7 @@ def init_db(db: Session):
         {"name": "В работе", "code": "in_progress"},
         {"name": "Завершено", "code": "done"}
     ]
-    
     for status_data in default_statuses:
         if not db.query(TaskStatus).filter(TaskStatus.code == status_data["code"]).first():
             db.add(TaskStatus(**status_data))
-    
     db.commit()
